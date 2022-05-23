93631713695b10d608106fed24f1907daa440090
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-05-23 21:07:15 +0530

    styledHero component added

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+import { RoomContext } from '../context'
+import StyledHero from '../components/StyledHero'
 
 export class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+    static contextType = RoomContext
     render() {
+        const { getRoom } = this.context
+        const room = getRoom(this.state.slug)
+        if (!room) {
+            return <div className="error">
+                <h3>no such room could be found</h3>
+                <Link to='/rooms' className='btn-primary'>
+                    back to rooms
+                </Link>
+            </div>
+        }
+        const { name, description, capacity, size, price, extras, breakfast, pets, images } = room
         return (
-            <div>Single Room page</div>
+            <>
+                <StyledHero img={images[0] || this.state.defaultBcg}>
+                    <Banner title={`${name} room`}>
+                        <Link to='/rooms' className='btn-primary'>
+                            back to rooms
+                        </Link>
+                    </Banner>
+                </StyledHero>
+
+            </>
         )
     }
 }
